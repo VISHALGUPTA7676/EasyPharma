31408c0b4359b3959894e3ed5fb5a90b0ff11f6a
Gupta Vishalkumar Ashokshah <107943646+VISHALGUPTA7676@users.noreply.github.com>
2023-08-14 21:32:04 +0530

    1c

---
+++ EasyPharma/src/main/java/com/EasyPharma/EasyPharmaApplication.java
@@ -0,0 +1,13 @@
+package com.EasyPharma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyPharmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyPharmaApplication.class, args);
+	}
+
+}
---
+++ EasyPharma/src/test/java/com/EasyPharma/EasyPharmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EasyPharma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyPharmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
